395291c1568df1ea99d9c01b53827c6ce9028fa6
MarsMoldobekov <marsmoldobekov13144@gmail.com>
2020-11-17 16:39:20 +0300

    Add neuron_net_system class

--- NeuralNetwork/neural_network.h
+++ NeuralNetwork/neural_network.h
@@ -3,6 +3,7 @@
 
 #include <numeric>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -25,12 +26,7 @@ public:
 
 	template<typename _Iv, typename _Ic>
 	static state_t calculate(_Iv val_b, _Iv val_e, _Ic coeff_b) {
-		auto val = inner_product(
-			val_b,
-			val_e,
-			coeff_b,
-			coeff_t(0)
-		);
+		auto val = inner_product(val_b, val_e, coeff_b, coeff_t(0));
 		return val > 0 ? state_t::UPPER_STATE : state_t::LOWER_STATE;
 	}
 };
@@ -39,5 +35,53 @@ typedef Neuron neuron_t;
 typedef neuron_t::state_t state_t;
 typedef vector<state_t> neurons_line;
 typedef vector<vector<neuron_t::coeff_t>> link_coeffs;
+
+class neuron_net_system {
+public:
+	const link_coeffs& _coeffs;
+
+	neuron_net_system(const link_coeffs& _coeffs) : _coeffs(_coeffs) {
+
+	}
+
+	bool do_step(neurons_line& line) {
+		bool value_changed = false;
+
+		neurons_line old_values(line.begin(), line.end());
+		link_coeffs::const_iterator it_coeffs = _coeffs.begin();
+
+		transform(line.begin(), line.end(), line.begin(),
+			[&old_values, &it_coeffs, &value_changed](state_t old_value) -> state_t {
+				auto new_value = neuron_t::calculate(
+					old_values.begin(),
+					old_values.end(),
+					begin(*it_coeffs++)
+				);
+				value_changed = (new_value != old_value) || value_changed;
+				return new_value;
+			}
+		);
+
+		return value_changed;
+	}
+
+	size_t _do(neurons_line& line) {
+		bool need_continue = true;
+		_steps_done = 0;
+		
+		while (need_continue) {
+			need_continue = do_step(line);
+			++_steps_done;
+		}
+
+		return _steps_done;
+	}
+
+	size_t steps_donw() const {
+		return _steps_done;
+	}
+private:
+	size_t _steps_done;
+};
 
 #endif /* NEURAL_NETWORK_H */
